08b7c69286f7c5115548f8c4de21b9eb1f5468c5
Swapnanil Saha <greatdreamblue4545@gmail.com>
2022-06-24 19:28:26 +0530

    Config update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import awsconfig from './aws-exports';
 import {Amplify, Auth, Storage } from 'aws-amplify';
 import { createNewCommentData, deleteTask, updateCommentData, getTaskbyId } from './jqlFunctions/TaskCommentsMapping';
 
-Amplify.configure({
+/*Amplify.configure({
   Auth: {
         identityPoolId: 'us-east-1:af6ffad4-71cc-472c-a04a-4a5ebe558834', //REQUIRED - Amazon Cognito Identity Pool ID
         region: 'us-east-1', // REQUIRED - Amazon Cognito Region
@@ -18,9 +18,19 @@ Amplify.configure({
             bucket: 'docsxyz170911-staging', //REQUIRED -  Amazon S3 bucket name
             region: 'us-east-1', //OPTIONAL -  Amazon service region
         }
-    }
-});
+    },
 
+    myAppConfig : {
+      
+      'aws_appsync_graphqlEndpoint': "https://xw6fycalcbhnfje2o67djkat3e.appsync-api.us-east-1.amazonaws.com/graphql",
+      'aws_appsync_region': 'us-east-1',
+      'aws_appsync_authenticationType': 'API_KEY',
+      'aws_appsync_apiKey': "da2-gqbt2cbmbndprga3kyaadlh5ba",
+      
+  }
+});*/
+
+Amplify.configure(awsconfig)
 
 
 function App() {
@@ -54,13 +64,15 @@ function App() {
         contentType: "image/png", // contentType is optional
       });
       console.log("data is", data)
+      const get = await Storage.get(data.key);
+      console.log(get);
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
   }
 
   const cdata = {
-    commentPath: "xya",
+    commentPath: "xyabb",
     filePath: "xtz",
     orderID: "abc"
   }
@@ -73,12 +85,13 @@ function App() {
   return (
     <div className="App">
       <h1>S3 test</h1>
+      <input type="file" onChange={onChange} /><br/><br/>
+
 
       <input id ="input" onChange={(Var)=>setfilename(Var.target.value)}/>
       <input id ="input" onChange={(Var1)=>setcontent(Var1.target.value)}/>
       <button onClick={dnTxtFile}>send text to s3</button><br/><br/>
       {/*<button onClick={()=>createNewInfo(hardData)}>create new info</button><br/><br/>*/}
-      <input type="file" onChange={onChange} />;
       <h1>order task</h1>
       <button onClick={() => createNewCommentData(cdata)}>Create new Task</button><br/><br/>
       <button onClick={() => updateCommentData()}>update new Task</button><br/><br/>
